7b264a5ead72f1131d9c80b924814f2b8d4ae64d
rafaelrfd1 <rafaelrfd18@gmail.com>
2026-03-22 15:48:16 -0300

    Versão projeto sem arquivos teste

--- teste_registro.py
+++
@@ -1,4 +0,0 @@
-from database import registrar_presenca_automatica
-
-print(registrar_presenca_automatica(16, "teste automatico 1"))
-print(registrar_presenca_automatica(16, "teste automatico 2"))
--- teste_url_foto.py
+++
@@ -1,7 +0,0 @@
-from database import obter_url_assinada_foto
-
-# troque pelo caminho real salvo no banco
-foto_path = "alunos/16/principal.jpg"
-
-url = obter_url_assinada_foto(foto_path, 300)
-print(url)
