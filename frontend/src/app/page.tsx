2231bab4871885a6a8f6085601ec4fd97566f866
rafaelrfd1 <rafaelrfd18@gmail.com>
2026-03-23 23:16:27 -0300

    Adiciona frontend Next ao projeto e organiza estrutura

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button }  from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Card, CardTitle }  from "@/components/ui/card"
+
+const Home =() => {
+    return(
+        <Card className="w-[30%] p-4 mx-auto mt-10">
+             <CardTitle>Card Title</CardTitle>
+            <Input  placeholder="Digite seu email aqui"/>
+            <Button className="cursor-pointer">Enviar </Button>
+        </Card>
+    )
+}
+
+export default Home
